db639f7909b7982245d683f0cb125142b81605d8
ZRunner <z.runner.mc@gmail.com>
2021-11-13 23:14:59 +0100

    add some API queries

--- server.js
+++ server.js
@@ -36,6 +36,26 @@ app.get("/api/categories", async (req, res) => {
     res.json(query);
 })
 
+app.get("/api/total-balance-by-day", async (req, res) => {
+    const query = await db.query("SELECT f.date, (SELECT ROUND(SUM(f2.cost), 2) FROM `flows` f2 WHERE f2.date <= f.date) as costs FROM `flows` f GROUP BY `date` ORDER BY `date` ASC;");
+    res.json(query);
+})
+
+app.get("/api/flows-by-day", async (req, res) => {
+    const query = await db.query("SELECT f.date, ROUND(SUM(f.cost), 2) as costs FROM `flows` f GROUP BY `date` ORDER BY `date` ASC;");
+    res.json(query);
+})
+
+app.get("/api/flows-by-day/:day", async (req, res) => {
+    // check for the date
+    if (!new Date(req.params.day).getDay()) {
+        res.status(400).send("Invalid date");
+        return;
+    }
+    const query = await db.query("SELECT f.date, ROUND(SUM(f.cost), 2) as costs FROM `flows` f WHERE f.date = ?;", [req.params.day]);
+    res.json(query[0].date ? query[0] : {date: req.params.day, costs: 0.0});
+})
+
 app.get("/api/flows", async (req, res) => {
     const query = await db.query("SELECT * FROM flows ORDER BY date DESC;");
     res.json(query);
@@ -51,8 +71,9 @@ app.post("/api/flows", async (req, res) => {
         res.status(400).send("Missing parameter");
         return;
     }
+    let query;
     try {
-        const query = await db.query("INSERT INTO `flows` (`id`, `name`, `cost`, `category`, `bank_account`, `date`) VALUES (NULL, ?, ?, ?, ?, ?)", [req.body.name, req.body.cost, req.body.category, req.body.bank_account, req.body.date])
+        query = await db.query("INSERT INTO `flows` (`id`, `name`, `cost`, `category`, `bank_account`, `date`) VALUES (NULL, ?, ?, ?, ?, ?)", [req.body.name, req.body.cost, req.body.category, req.body.bank_account, req.body.date])
     } catch (err) {
         return next(err);
     }
@@ -60,8 +81,9 @@ app.post("/api/flows", async (req, res) => {
 })
 
 app.delete("/api/flows/:id", async (req, res) => {
+    let query;
     try {
-        const query = await db.query("DELETE FROM `flows` WHERE id = ?", [req.params.id]);
+        query = await db.query("DELETE FROM `flows` WHERE id = ?", [req.params.id]);
     } catch (err) {
         return next(err);
     }
