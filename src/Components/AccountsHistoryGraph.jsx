d448c8e84a4478252e4ba0ebe16fd1bacc57b9ba
ZRunner <z.runner.mc@gmail.com>
2021-11-15 19:28:49 +0100

    new graph

---
+++ src/Components/AccountsHistoryGraph.jsx
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+
+import { AppContext } from '../AppContext';
+
+const options = {
+    pointBackgroundColor: '#fff',
+    radius: 2,
+    normalized: true,
+    spanGaps: true,
+}
+
+class AccountsHistoryGraph extends Component {
+
+    static contextType = AppContext
+
+    constructor(props, context) {
+        super(props);
+        this.context = context;
+
+        this.state = {
+            data: this.createDataset(new Map())
+        }
+        this.previous_expenses = [];
+        this.previous_accounts = [];
+    }
+
+    componentDidMount() {
+        console.debug("MOUNTED")
+        this.componentDidUpdate();
+    }
+
+    componentDidUpdate() {
+        if (this.context.bank_accounts.length === 0) return;
+        if (this.previous_expenses !== this.context.expenses || this.previous_accounts !== this.props.accounts) {
+            this.constructData();
+            this.previous_expenses = this.context.expenses;
+            this.previous_accounts = this.props.accounts;
+        }
+    }
+
+    formatDate(raw_date) {
+        return new Date(raw_date).toLocaleDateString();
+    }
+
+    getAccountsMap() {
+        let result = new Map(this.props.accounts.map(id => [id, this.context.getAccount(id)]))
+        result.set('total', { name: 'total', 'color': "#009926" })
+        return result
+    }
+
+    createDataset(historics) {
+        const accountsMap = this.getAccountsMap();
+        let obj = { datasets: [] }
+        for (let [acc_id, value] of historics) {
+            const acc = accountsMap.get(acc_id)
+            obj.datasets.push({
+                label: acc.name,
+                data: value,
+                fill: false,
+                borderColor: acc.color,
+                backgroundColor: acc.color,
+            })
+        };
+        return obj;
+    }
+
+    constructData() {
+        const accountsMap = this.getAccountsMap();
+
+        let day = new Date(this.props.startDate);
+        let result = new Map(this.props.accounts.map(acc => [acc, []]))
+        result.set('total', [])
+
+        let balancePerAcc = new Map(this.props.accounts.map(acc => [
+            acc,
+            accountsMap.get(acc).initial_balance
+        ]))
+        let today = this.props.endDate ? new Date(this.props.endDate) : new Date();
+
+        const addValue = (acc_name, day, value) => {
+            let res = result.get(acc_name);
+            res.push({
+                x: this.formatDate(day),
+                y: value
+            })
+        }
+
+        while (day < today) {
+            let dayDiff = new Map(this.props.accounts.map(acc => {
+                return [acc, balancePerAcc.get(acc)]
+            }))
+            for (let exp of this.context.getFlowsOfDay(day)) {
+                if (this.props.accounts.includes(exp.bank_account)) {
+                    dayDiff.set(exp.bank_account, dayDiff.get(exp.bank_account) + exp.cost);
+                }
+            }
+
+            let total = 0;
+            dayDiff.forEach((value, acc) => {
+                addValue(acc, day, value);
+                balancePerAcc.set(acc, value);
+                total += value
+            })
+            addValue('total', day, total);
+
+            day.setDate(day.getDate() + 1);
+        }
+
+        this.setState({
+            data: this.createDataset(result)
+        });
+    }
+
+    render() {
+        return (
+            <>
+                {this.state.data.datasets.length > 0 ?
+                    <Line data={this.state.data} options={options} /> :
+                    null
+                }
+            </>
+        )
+    }
+}
+
+export default AccountsHistoryGraph
--- src/Components/MiscSelects.jsx
+++ src/Components/MiscSelects.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../AppContext';
+import Select from 'react-select'
 
 export const CategorySelect = (props) => {
 
@@ -29,4 +30,62 @@ export const AccountSelect = (props) => {
             ))}
         </select>
     )
+}
+
+export class AccountsSelect extends React.Component {
+
+    static contextType = AppContext;
+
+    constructor(props, context) {
+        super(props);
+        this.context = context;
+
+        const opt = this.getOptions();
+        this.state = {
+            selectedAccounts: opt,
+            options: opt
+        }
+        this.props.onUpdate(this.state.selectedAccounts);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.initializedAccounts = false;
+        this.saved_bank_accounts = this.context.bank_accounts;
+    }
+
+    componentDidUpdate() {
+        if (this.context.bank_accounts !== this.saved_bank_accounts) {
+            this.setState({options: this.getOptions()}, () => {
+                this.props.onUpdate(this.state.selectedAccounts);
+            })
+            this.saved_bank_accounts = this.context.bank_accounts;   
+        }
+    }
+
+    getOptions() {
+        const options = Array.from(this.context.bank_accounts, acc => {return {
+            value: acc.id,
+            label: acc.name
+        }})
+        if (!this.initializedAccounts && options.length > 0) {
+            this.setState({selectedAccounts: options});
+            this.initializedAccounts = true;
+        }
+        return options;
+    }
+
+    handleChange(new_array) {
+        this.setState({
+            selectedAccounts: new_array
+        })
+        if (this.props.onUpdate) {
+            this.props.onUpdate(new_array);
+        }
+    }
+
+    render() {
+        return (
+            <Select options={this.state.options} isMulti={true} value={this.state.selectedAccounts} onChange={this.handleChange} {...this.props} />
+        )
+    }
+
 }
