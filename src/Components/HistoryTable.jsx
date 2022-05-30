5eb1a58d46cf760772d2cc51c607ae3c5c9cf7f2
ZRunner <z.runner.mc@gmail.com>
2022-05-30 13:41:50 +0200

    fix history table

--- src/Components/HistoryTable.jsx
+++ src/Components/HistoryTable.jsx
@@ -54,7 +54,7 @@ class HistoryTable extends Component {
         let prev_dat = new Date(day);
 
         let i = 0;
-        while (day < today && i < 400) {
+        while (day <= today && i < 400) {
             for (let exp of this.context.getFlowsOfDay(day)) {
                 if (this.props.accounts.includes(exp.bank_account)) {
                     dayState.set(exp.bank_account, dayState.get(exp.bank_account)+exp.cost);
