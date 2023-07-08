ae69d7b80ed2e1825de1119c2d31fdf23f8b07e7
ZRunner <z.runner.mc@gmail.com>
2023-07-08 10:08:09 -0400

    fix mysql timeout

--- server.ts
+++ server.ts
@@ -34,7 +34,10 @@ mysql.createPool({
     port: (Number(process.env.DB_PORT || 3306)),
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
 })
     .then(pool => {
         console.debug("Database pool created")
